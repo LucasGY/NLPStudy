2d636cd58aacbac9b4c295454099ae6a0b8f2351
gouyue <gouyue@shouqianba.com>
2022-06-07 13:49:43 +0800

    initialize

---
+++ wordEmbedding/creatProject.sh
@@ -0,0 +1,18 @@
+mkdir data eda models saved utils train deploy test log temp config
+touch ./models/__init__.py 
+touch ./utils/__init__.py 
+touch ./log/__init__.py 
+touch ./run.py 
+touch ./requirements.txt
+
+touch ./config/.gitkeep
+touch ./data/.gitkeep
+touch ./deploy/.gitkeep
+touch ./eda/.gitkeep
+touch ./log/.gitkeep
+touch ./saved/.gitkeep
+touch ./temp/.gitkeep
+touch ./test/.gitkeep
+touch ./train/.gitkeep
+
+chmod -R 0777 *
